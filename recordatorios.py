283afccdb244a386115070dbbb24526696ed878d
Jorge201105 <jimunozacuna@gmail.com>
2025-05-28 17:18:11 +0000

    terminado tercer desafío

--- recordatorios.py
+++ recordatorios.py
@@ -4,5 +4,26 @@ recordatorios = [['2021-01-01', "11:00", "Levantarse y ejercitar"],
  ['2021-09-18', "16:00", "Ramadas"],
  ['2021-12-25', "00:00", "Navidad"]]
 
-# Output
+
+# se ingresa el día 2 de febrero como fecha de empezar el año, en las respuestas aparece el 01 de Febrero pero la pregunta habla del 02 de Febrero(detalle)
+empezar_año= ["2021-02-02","06:00","Empezar el año"]
+recordatorios.insert(1,empezar_año)
+
+# Se cambia día en el indice 3 de la lista
+recordatorios[3] = ["2021-07-16","13:00","No hacer nada es feriado"]
+
+# Se comprueba que lo que estamos borrando sea el ía del trabajo y después lo dejamos como comentario
+# print(recordatorios[2])
+recordatorios.pop(2)
+
+# Se agregan ambas cenas en las posiciones según orden de fecha.
+cena_navidad = ["2021-12-24","22:00","Cena de Navidad"]
+cena_año_nuevo = ["2021-12-31","22:00","Cena de año nuevo"]
+
+recordatorios.insert(4,cena_navidad)
+recordatorios.insert(6,cena_año_nuevo)
+
+
+
+# Output, imprime a lo largo, pero coincidiendo con la respuesta
 print(recordatorios)
