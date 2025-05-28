f79ad762c889a08438a9f03b99e7386f32466a2b
Jorge201105 <jimunozacuna@gmail.com>
2025-05-28 13:36:34 +0000

    se termina segundo desafío

---
+++ word_count.py
@@ -0,0 +1,20 @@
+import sys
+
+with open("lorem_ipsum.txt","r") as file:
+    texto=file.read()
+
+numero_caracteres=len(set(texto))
+# Se realiza el chequeo de los caracteres encontrados y después se deja como comentario
+# print(set(texto))
+#print(numero_caracteres)
+
+
+palabras = texto.split(" ")
+
+#Se chequea el numero de palabras encontradas con print pero despues se deja como comentario
+# print(palabras)
+numero_palbras = len(set(palabras))
+#print(numero_palbras)
+
+print(f"El úmero de carcateres distintos es : {numero_caracteres}")
+print(f"El numero de palabras distintas es : {numero_palbras}")
